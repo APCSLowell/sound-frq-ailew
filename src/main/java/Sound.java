87acfac6e23ab214b531c5ffe0a252a707e73d79
ailew <143014764+ailew@users.noreply.github.com>
2024-04-20 03:27:53 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -1,49 +1,44 @@
 import java.util.*;
-public class Sound
-{
-  /** the array of values in this sound; guaranteed not to be null */
-  int[] samples;
 
+public class Sound {
+    /** the array of values in this sound; guaranteed not to be null */
+    int[] samples;
 
-
-  /** Changes those values in this sound that have an amplitude greater than limit.
-   *  Values greater than limit are changed to limit.
-   *  Values less than -limit are changed to -limit.
-   *  @param limit the amplitude limit
-   *         Precondition: limit >= 0
-   *  @return the number of values in this sound that this method changed
-   */
-  public int limitAmplitude(int limit)
-  {  
-    int count = 0;
-    for(int i = 0; i < samples.length; i++){
-      if(samples[i] > limit){
-        samples[i] = limit;
-        count++;
-      } else if (samples[i] < -limit){
-        samples[i] = -limit;
-        count++
-      }
+    /** Changes those values in this sound that have an amplitude greater than limit.
+     *  Values greater than limit are changed to limit.
+     *  Values less than -limit are changed to -limit.
+     *  @param limit the amplitude limit
+     *         Precondition: limit >= 0
+     *  @return the number of values in this sound that this method changed
+     */
+    public int limitAmplitude(int limit) {  
+        int count = 0;
+        for (int i = 0; i < samples.length; i++) {
+            if (samples[i] > limit) {
+                samples[i] = limit;
+                count++;
+            } else if (samples[i] < -limit) {
+                samples[i] = -limit;
+                count++;
+            }
+        }
+        return count;
     }
-    return count;
-    return 0;
-  }
 
-
-
-  /** Removes all silence from the beginning of this sound.
-   *  Silence is represented by a value of 0.
-   *  Precondition: samples contains at least one nonzero value
-   *  Postcondition: the length of samples reflects the removal of starting silence
-   */
-  public void trimSilenceFromBeginning()
-  {
-    int i = 0;
-    while( samples[n] == 0)
-      n++;
-    int[] newSamples = new int[samples.length - n];
-    for(int i = 0; i < newSamples.length; i++)
-      newSamples[i] = samples[i+n];
-    samples = newSamples;
-  }
+    /** Removes all silence from the beginning of this sound.
+     *  Silence is represented by a value of 0.
+     *  Precondition: samples contains at least one nonzero value
+     *  Postcondition: the length of samples reflects the removal of starting silence
+     */
+    public void trimSilenceFromBeginning() {
+        int n = 0;
+        while (samples[n] == 0) {
+            n++;
+        }
+        int[] newSamples = new int[samples.length - n];
+        for (int i = 0; i < newSamples.length; i++) {
+            newSamples[i] = samples[i + n];
+        }
+        samples = newSamples;
+    }
 }
